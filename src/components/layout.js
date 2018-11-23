bffd6d64d9ef83d3455641dc5e46da31a86d4c44
Alessandro Arnodo <alessandro@arnodo.net>
2018-11-23 16:00:23 +0100

    404

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, Component } from 'react'
 import { MDXProvider } from '@mdx-js/tag'
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
@@ -8,6 +8,17 @@ import Header from './header'
 
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
+
+if (global.document) {
+  const info = [
+    `You can find the code here: https://github.com/vesparny/site`,
+    `Cheers! 😀`
+  ]
+
+  for (const message of info) {
+    console.log(message)
+  }
+}
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -27,70 +38,85 @@ export const Root = props => (
   />
 )
 
-export default ({ children, location, showWritingLink }) => (
-  <StaticQuery
-    query={graphql`
-      query LayoutQuery {
-        site {
-          siteMetadata {
-            title
-            description
-            siteUrl
+export default class Layout extends Component {
+  componentDidMount() {
+    // analytics here
+  }
+  render() {
+    const { children, location, showWritingLink } = this.props
+    return (
+      <StaticQuery
+        query={graphql`
+          query LayoutQuery {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+              }
+            }
           }
-        }
-      }
-    `}
-    render={({ site }) => (
-      <Fragment>
-        <Helmet
-          defaultTitle={site.siteMetadata.title}
-          titleTemplate={`%s | ${site.siteMetadata.title}`}>
-          <meta name="description" content={site.siteMetadata.description} />
-          <link
-            rel="canonical"
-            href={`${site.siteMetadata.siteUrl}${location.pathname}`}
-          />
-          <meta property="og:title" content={site.siteMetadata.title} />
-          <meta
-            property="og:url"
-            content={`${site.siteMetadata.siteUrl}${location.pathname}`}
-          />
-          <meta
-            property="og:description"
-            content={site.siteMetadata.description}
-          />
-          <meta
-            property="og:image"
-            content={`${site.siteMetadata.siteUrl}/site-thumbnail.png`}
-          />
-          <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
-          <html lang="en" />
-        </Helmet>
-        <GlobalStyle />
-        <ThemeProvider theme={theme}>
-          <MDXProvider
-            components={{
-              p: P.P,
-              blockquote: P.Blockquote,
-              h3: P.H3,
-              a: P.A,
-              inlineCode: P.InlineCode,
-              pre: P.Pre,
-              hr: P.Hr,
-              li: P.Li,
-              ul: P.Ul,
-              img: P.Img,
-              code: P.Code
-            }}>
-            <Root>
-              <P.Text fontFamily="sans">
-                <Header showWritingLink={showWritingLink} />
-                {children}
-              </P.Text>
-            </Root>
-          </MDXProvider>
-        </ThemeProvider>
-      </Fragment>
-    )}
-  />
-)
+        `}
+        render={({ site }) => (
+          <Fragment>
+            <Helmet
+              defaultTitle={site.siteMetadata.title}
+              titleTemplate={`%s | ${site.siteMetadata.title}`}>
+              <meta
+                name="description"
+                content={site.siteMetadata.description}
+              />
+              <link
+                rel="canonical"
+                href={`${site.siteMetadata.siteUrl}${location.pathname}`}
+              />
+              <meta property="og:title" content={site.siteMetadata.title} />
+              <meta
+                property="og:url"
+                content={`${site.siteMetadata.siteUrl}${location.pathname}`}
+              />
+              <meta
+                property="og:description"
+                content={site.siteMetadata.description}
+              />
+              <meta
+                property="og:image"
+                content={`${site.siteMetadata.siteUrl}/site-thumbnail.png`}
+              />
+              <link
+                rel="shortcut icon"
+                type="image/x-icon"
+                href="/favicon.ico"
+              />
+              <html lang="en" />
+            </Helmet>
+            <GlobalStyle />
+            <ThemeProvider theme={theme}>
+              <MDXProvider
+                components={{
+                  p: P.P,
+                  blockquote: P.Blockquote,
+                  h3: P.H3,
+                  a: P.A,
+                  inlineCode: P.InlineCode,
+                  pre: P.Pre,
+                  hr: P.Hr,
+                  li: P.Li,
+                  ul: P.Ul,
+                  img: P.Img,
+                  code: P.Code
+                }}>
+                <Root>
+                  <P.Text fontFamily="sans">
+                    <Header showWritingLink={showWritingLink} />
+                    {children}
+                  </P.Text>
+                </Root>
+              </MDXProvider>
+            </ThemeProvider>
+          </Fragment>
+        )}
+      />
+    )
+  }
+}
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import * as P from '../components/primitives'
+import Layout from '../components/layout'
 
-export default () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn't exist... the sadness.</p>
-  </div>
+export default ({ location }) => (
+  <Layout location={location}>
+    <P.Flex
+      css={{
+        height: 'calc(100vh - 120px)'
+      }}
+      alignItems="center"
+      justifyContent="center">
+      <P.Box>
+        <P.Card
+          fontSize={4}
+          as="span"
+          pr={2}
+          color="near-black"
+          borderRight={1}
+          borderColor="silver">
+          404
+        </P.Card>
+        <P.Text fontSize={4} as="span" color="silver" pl={2}>
+          Ouch, this page does not exist, the sadness 😩
+        </P.Text>
+      </P.Box>
+    </P.Flex>
+  </Layout>
 )
